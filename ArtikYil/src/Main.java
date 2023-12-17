1b9a28841e973851b759287cb9151d1dc683173a
test <omerfaruk38baran@gmail.com>
2023-12-17 22:34:21 +0300

    Kullanıcıdan alınan girdinin en küçük ve en büyük değerini ekrana yazdırır

--- ArtikYil/src/Main.java
+++ ArtikYil/src/Main.java
@@ -1,18 +1,37 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        /* 4 e bölünecek ve 100 ün katlarında 400'ün katı olarak sayılacak */
-
         Scanner input = new Scanner(System.in);
-        System.out.println("Yıl Girini :");
-        int year = input.nextInt();
 
-        if ((year % 4 == 0) && year %100 !=0 || (year % 400 == 0)) {
-            System.out.println(year + " Artık Yıldır.");
-        } else {
-            System.out.println(year + " Artık Yıl Değildir.");
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int girdi = input.nextInt();
+        int enBuyukSayi = 0, enKucukSayi = 0;
+
+
+        // en büyük ve en küçük sayıyı tutacak değişkenlere 0 değerini girdik
+        // Alt tarafta işlerken hata veriyordu.
+
+
+        for (int i = 1; i <= girdi; i++) {
+            System.out.print(i + ". Sayıyı Giriniz: ");
+            int sayi = input.nextInt();
+            if (i == 1) {
+
+                enBuyukSayi = sayi;
+                enKucukSayi = sayi;
+
+            } else {
+                if (sayi > enBuyukSayi) {
+                    enBuyukSayi = sayi;
+                } else if (sayi < enKucukSayi) {
+                    enKucukSayi = sayi;
+
+                }
+            }
         }
+        System.out.println("En büyük sayı : " + enBuyukSayi);
+        System.out.println("En kücük sayı : " + enKucukSayi);
     }
-}
 
+}
