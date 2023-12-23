6551cdb9f973411e4cee2c6101a55a341cc5b7be
test <omerfaruk38baran@gmail.com>
2023-12-23 12:40:34 +0300

    Öğrenci Not Sistemi

---
+++ NotSistemi/src/Course.java
@@ -0,0 +1,40 @@
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int verbal;
+    int note;
+    int verbalAvarageRatio;
+
+
+
+
+
+    public Course(String name, String code, String prefix , int verbalAvarageRatio) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+        this.verbal =0;
+        this.verbalAvarageRatio = verbalAvarageRatio;
+        }
+
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+}
